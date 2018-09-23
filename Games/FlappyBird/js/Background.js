07c38663655cf63888c27a59c1ea0d7ab0471193
Sagar Acharya <sagarach65@gmail.com>
2018-09-23 19:23:33 +0545

    completed flappy bird game minor adjustments left

--- Games/FlappyBird/js/Background.js
+++ Games/FlappyBird/js/Background.js
@@ -1,17 +1,21 @@
-function Background() {
+class Background {
+    constructor() {
 
-    this.showBackgroundImage = () => {
+    }
+
+    showBackgroundImage() {
         let backgroundImg = new Image();
         backgroundImg.src = 'images/background.png';
         ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
     }
-    this.showFloor = () => {
+
+    showFloor() {
         let backgroundImg = new Image();
         backgroundImg.src = 'images/floor2.png';
         ctx.drawImage(backgroundImg, 0, canvas.height - 16, canvas.width, 12);
     }
 
-    this.showFloor2 = () => {
+    showFloor2() {
         let backgroundImg = new Image();
         backgroundImg.src = 'images/floor.png';
         ctx.drawImage(backgroundImg, 0, canvas.height - 12, canvas.width, 12);
--- Games/FlappyBird/js/Bird.js
+++ Games/FlappyBird/js/Bird.js
@@ -1,44 +1,51 @@
-function Bird() {
-    this.x = 50;
-    this.y = canvas.height / 2 - 30;
-    this.gravity = 1;
-    this.score = 0;
+class Bird {
+    constructor() {
+        this.x = 50;
+        this.y = canvas.height / 2 - 30;
+        this.gravity = 0.05;
+        this.score = 0;
+        this.velocity = 0;
+    }
 
-    this.show = () => {
+    show() {
         let birdImg = new Image();
         birdImg.src = 'images/bird.png';
         ctx.drawImage(birdImg, this.x, this.y, 30, 11);
     }
 
-    this.updatePerGravity = () => {
-        this.y += this.gravity;
+    updatePerGravity() {
+        this.velocity += this.gravity;
+        this.y += this.velocity;
     }
-    this.checkTopCollision = () => {
+
+    checkTopCollision() {
         //here 11 is the height of the bird and 30 is the width of the bird
         if (this.y + 11 < 0) {
-            this.gravity = 0;
+            this.velocity = 0;
             //    console.log('game over');
-            this.showGameOver();
+            isOver = true;
         }
     }
-    this.checkBottomCollision = () => {
-        if (this.y + 11 > canvas.height - 16) {
-            this.gravity = 0;
-            this.showGameOver();
+
+    checkBottomCollision() {
+        if (this.y + 11 > canvas.height - 14) {
+            this.velocity = 0;
+            isOver = true;
             //  console.log('game over');
         }
     }
-    this.jumpBird = () => {
+
+    jumpBird() {
         //increases the coordinates of the bird when space is pressed
-        this.y -= 25;
+        this.velocity = -0.9;
     }
-    this.updateScore = () => {
+
+    updateScore() {
         this.score++;
     }
 
-    this.showGameOver = () => {
-        ctx.fillText("                         Score:" + this.score, 10, canvas.height / 2 - 20);
-        ctx.fillText("                         GAME OVER!!       ", 10, canvas.height / 2);
-        ctx.fillText("     RESTART THE PAGE TO TRY AGAIN", 10, canvas.height / 2 + 20);
+    showScore() {
+        ctx.fillText("Score:" + this.score, canvas.width - 50, 10);
     }
+
 }
